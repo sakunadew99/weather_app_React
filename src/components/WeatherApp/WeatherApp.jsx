28eda65e319ef7b44a90f564f62cf64c5708e68e
Sakuna Dewanarayana <68386969+sakunadew99@users.noreply.github.com>
2024-04-08 07:18:27 +0530

    add background images for weather changes

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './WeatherApp.css'
 
 import search_icon from '../assets/search.png';
@@ -10,11 +10,17 @@ import snow_icon from '../assets/snow.png';
 import wind_icon from '../assets/wind.png';
 import humidity_icon from '../assets/humidity.png';
 
-const WeatherApp = () => {
+// Import background images for different weather conditions
+import sunny_bg from '../assets/sunny.jpeg';
+import cloudy_bg from '../assets/cloudy.jpg';
+import rainy_bg from '../assets/rainy1.webp';
+import snowy_bg from '../assets/snowy2.jpg';
 
+const WeatherApp = () => {
   let api_key = "d6a30441f712beb853d68f31a8101e74";
 
-  //const [wicon,setWicon] = useState("cloud_icon");
+  const [wicon, setWicon] = useState(cloud_icon); // Default weather icon
+  const [bgImage, setBgImage] = useState(cloudy_bg); // Default background image
 
   const search = async () => {
     const element = document.getElementsByClassName('cityInput');
@@ -26,6 +32,7 @@ const WeatherApp = () => {
     let response = await fetch(url);
     let data = await response.json();
 
+    // Update weather data
     const humidity = document.getElementsByClassName("humidity-percent");
     const wind = document.getElementsByClassName("wind-rate");
     const temp = document.getElementsByClassName("weather-temp");
@@ -33,55 +40,69 @@ const WeatherApp = () => {
 
     humidity[0].innerHTML = data.main.humidity+"%";
     wind[0].innerHTML = data.wind.speed+" km/h";
-    temp[0].innerHTML = data.main.temp +" C";
+    temp[0].innerHTML = data.main.temp.toFixed(1) +" °C";
     location[0].innerHTML = data.name;
 
-    /*if(data.weather[0].icon==="01d" || data.weather[0].icon==="01n"){
-      setWicon(clear_icon);
+    // Update weather icon and background image based on weather condition
+    switch(data.weather[0].icon) {
+      case "01d":
+      case "01n":
+        setWicon(clear_icon);
+        setBgImage(sunny_bg);
+        break;
+      case "02d":
+      case "02n":
+      case "03d":
+      case "03n":
+        setWicon(cloud_icon);
+        setBgImage(cloudy_bg);
+        break;
+      case "04d":
+      case "04n":
+      case "09d":
+      case "09n":
+      case "10d":
+      case "10n":
+        setWicon(rain_icon);
+        setBgImage(rainy_bg);
+        break;
+      case "13d":
+      case "13n":
+        setWicon(snow_icon);
+        setBgImage(snowy_bg);
+        break;
+      default:
+        setWicon(cloud_icon);
+        setBgImage(cloudy_bg);
     }
-    else if(data.weather[0].icon==="02d" || data.weather[0].icon==="02n"){
-      setWicon(cloud_icon);
+  }
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      search();
     }
-    else if(data.weather[0].icon==="03d" || data.weather[0].icon==="03n"){
-      setWicon(drizzle_icon);
-    }
-    else if(data.weather[0].icon==="04d" || data.weather[0].icon==="04n"){
-      setWicon(drizzle_icon);
-    }
-    else if(data.weather[0].icon==="09d" || data.weather[0].icon==="09n"){
-      setWicon(rain_icon);
-    }
-    else if(data.weather[0].icon==="10d" || data.weather[0].icon==="10n"){
-      setWicon(rain_icon);
-    }
-    else if(data.weather[0].icon==="13d" || data.weather[0].icon==="13n"){
-      setWicon(snow_icon);
-    }
-    else{
-      setWicon(clear_icon);
-    }*/
   }
 
   return (
-    <div className="container">
+    <div className="container" style={{ backgroundImage: `url(${bgImage})` }}>
       <div className="top-bar">
-        <input type="text" className="cityInput" placeholder="search" />
+        <input type="text" className="cityInput" placeholder="search" onKeyPress={handleKeyPress} />
 
-        <div className="search-icon" onClick={() => {search()}}>
+        <div className="search-icon" onClick={search}>
           <img src={search_icon} alt="" />
         </div>
       </div>
       <div className='weather-image'>
-        <img src={cloud_icon} alt="" />
+        <img src={wicon} alt="" /> {/* Display the dynamic icon */}
       </div>
-      <div className='weather-temp'>24 C</div>
+      <div className='weather-temp'>24 °C</div>
       <div className='weather-location'>London</div>
       <div className='data-container'>
         <div className='element'>
           <img src={humidity_icon} alt="" className='icon' />
           <div className='data'>
-          <div className='humidity-percent'>64%</div>
-            <div class="text">Humidity</div>
+            <div className='humidity-percent'>64%</div>
+            <div className="text">Humidity</div>
           </div>
         </div>
 
@@ -89,7 +110,7 @@ const WeatherApp = () => {
           <img src={wind_icon} alt="" className='icon' />
           <div className='data'>
             <div className="wind-rate">18 km/h</div>
-            <div class="text">Wind Speed</div>
+            <div className="text">Wind Speed</div>
           </div>
         </div>
       </div>
@@ -97,4 +118,4 @@ const WeatherApp = () => {
   )
 }
 
-export default WeatherApp
+export default WeatherApp;
